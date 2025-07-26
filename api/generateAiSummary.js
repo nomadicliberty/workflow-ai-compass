9eb5bbd344d6249725ab8fa6491a0e7ec2607b23
nomadicliberty <nomadicliberty@gmail.com>
2025-07-26 07:20:57 -0400

    Add log to debug incoming HTTP method

--- api/generateAiSummary.js
+++ api/generateAiSummary.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+console.log("📥 Incoming method:", req.method); // 👈 Add this line
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
